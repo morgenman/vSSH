2c16cd7fb23bd2e2d54fb5fbb77f90a3b46dcaa8
morgendc203 <morgendc203@potsdam.edu>
2021-10-23 12:32:38 -0400

    oo structuring, keep searching if not exec

--- src/main/vsh.cpp
+++ src/main/vsh.cpp
@@ -11,36 +11,70 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-string getPath(string in);
-void getExec(fs::path path);
+fs::path getPath(string in);
+fs::path getPath(string in, int index);
+bool getExec(fs::path path);
+bool run(string in);
 
 vector<string> _path = {".", "/usr/local/bin", "/usr/bin", "/bin"};
 
-string getPath(string in) {
-  string path = "";
+fs::path getPath(string in) {
   fs::path p;
+  for (size_t i = 0; i < _path.size(); i++) {
+    p = getPath(in, (int)i);
+    if (p.string() != "$")
+      if (getExec(p)) return p;
+  }
+  return p;
+}
+
+/**
+ *
+ * @param  {string} in :
+ * @param  {int} index : index of path out of * existing paths * .
+ * That is, if ls is found in both /usr/local/bin and /bin:
+ * getPath("ls", 0) -> /usr/local/bin/ls
+ * getpath("ls", 1) -> /bin/ls
+ * @return {fs::path}  :
+ */
+fs::path getPath(string in, int index) {
+  fs::path p = "$";
+  int i = 0;
   for (auto dir : _path) {
-    p = dir.append("/").append(in);
+    if (p.string() == "$" && i == index) p = dir.append("/").append(in);
 
     printf("checking %s\n", p.c_str());
 
     if (exists(p)) {
       printf("found %s in %s\n", in.c_str(), dir.c_str());
-      getExec(p);
-    } else {
-    }
+      i++;
+    } else
+      p = "$";
   }
-  return p.string();
+  return p;
 }
 
-void getExec(fs::path path) {
+bool getExec(fs::path path) {
   // TODO: Explain this
   // https://stackoverflow.com/questions/5719694/how-to-check-if-file-is-executable-in-c
   if (!access(path.c_str(), X_OK)) {
     printf("file is marked as executable\n");
+    return true;
   } else {
     printf("file is marked as not executable\n");
+    return false;
   }
+}
+
+bool run(string in) {
+  fs::path path = getPath(in);
+  if (path == "$") {
+    printf("Error, no executables found for %s\n", in.c_str());
+  } else if (getExec(path)) {
+  } else {
+    printf("Error, file is not marked as executable\n");
+  }
+  return false;
 }
 
 /**
@@ -48,7 +82,7 @@ void getExec(fs::path path) {
  */
 int main(int argc, char *argv[]) {
   string cmd = "ls";
-  string path = getPath("ls");
+  fs::path path = getPath("ls");
 
   string p1 = "lepton";
   string p2 = "meson";
